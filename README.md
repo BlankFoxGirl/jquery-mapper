8bf13968bce06a46c7b8a448752fba04a2faa2d7
Sarah Jabado <sarah@jabado.com.au>
2019-09-29 00:12:34 +0800

    Minor styling of location control display elements. Inclusion of icon pin.svg. Updates to readme. Additional SCSS Vars. Population of location data.

--- README.md
+++ README.md
@@ -19,4 +19,31 @@ This will simply render a google map into the bound element with not much else.
 - Locations
 This will render a google map with controls located to the right hand side, the controls will be populated with locations from the supplied option `Locations` as an array of objects.
 
+# Locations Object Structure;
+The locations property is structured as an array of objects using the following format;
+```
+{
+    name: 'My Location',
+    text: 'Some cool text...',
+    address: '189 Appleberry Way',
+    phone: '08 12134 5679',
+    lat: -33.782066,
+    lng: 151.101867,
+    icon: 'path_to_image_override',
+}
+```
+| Property | Type | Details |
+| ---      | ---  | ------  |
+| name     | String | The name of the location you wish to display, this will be the location's `title` |
+| text     | String | A brief description of the location you wish to display. |
+| address  | String | The address of the location, used in Geolocation and displayed in the controls. |
+| phone    | String | The phone number of the location, this will be formatted to a `TEL` Link. |
+| lat      | Float  | The optional latitude of the location, used to override address geolocation. |
+| lng      | Float  | The optional longitude of the location, used to override address geolcation. |
+| icon     | String | An optional URL / URI to an image that will be displayed in place of the default location SVG icon specified by `$location-item-icon` in the src/scss/variables.scss file. |
+
+
+# Credits;
+- Icons provided by freepik / flaticon.
+
 Author: Sarah Jabado @ Jabado Pty Ltd. 2019
--- src/js/jquery.mapper.js
+++ src/js/jquery.mapper.js
@@ -22,6 +22,14 @@
                     phone: '08 12134 5678',
                     lat: -33.863276,
                     lng: 151.107977,
+                },
+                {
+                    name: 'My Location 2',
+                    text: 'We sell cool stuff.',
+                    address: '189 Appleberry Way',
+                    phone: '08 12134 5679',
+                    lat: -33.782066,
+                    lng: 151.101867,
                 }
             ],
         };
@@ -36,7 +44,11 @@
         this.mapMode = () => {
             switch (this.settings.Mode) {
                 case "Locations":
-                    this.append('<div class="jquery-mapper-controls" id="MapControls">lorem</div>');
+                    
+                    var container = document.createElement('div');
+                    container.classList = 'jquery-mapper-controls';
+                    container.id = 'MapControls';
+                    var children = '';
                     $.each(this.settings.Locations, (index, item) => {
                         // Google map drop pin marker.
                         var latlng = new google.maps.LatLng(
@@ -53,12 +65,14 @@
                             animation: google.maps.Animation.DROP,
                           });
                           this.markers.push(marker);
+                          var icon = '';
+                          if (typeof item.icon !== 'undefined') {
+                            icon = '<img src="' + item.icon + '" alt="' + item.name + '" title="' + item.name + '">';
+                          }
+                          children = children + '<div class="location-item' + ((icon !== '') ? ' icon-supplied' : '') + '">' + icon + '<span class="title">' + item.name + '</span><span class="phone">' + item.phone + '</span><span class="address">' + item.address + '</span><span class="text">' + item.text + '</span></div>';
                           // Populate the controls with clickable options.
-                          var d = document.createElement('div');
-                          $(d).addClass = 'location-item';
-                          $(d).html(item.title);
-                          $(".jquery-mapper-controls").append(d);
-                    });
+                        });
+                        this.append('<div class="jquery-mapper-controls" id="MapControls">' + children + '</div>');
                     break;
             }
         }
